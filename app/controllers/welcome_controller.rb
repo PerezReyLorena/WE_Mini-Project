939885da7819388b87572c4649e800596d20f8a8
Sveta Krasikova <contact@svetakrasikova.eu>
2014-10-23 17:26:29 +0200

    Add associations between User and Partnership; generate User controller.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    if current_user.present?
+      render 'users/index'
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :partnerships_as_player1, :class_name => 'Partnership', foreign_key: 'player1'
+  has_many :partnerships_as_player2, :class_name => 'Partnership', foreign_key: 'player2'
 end
