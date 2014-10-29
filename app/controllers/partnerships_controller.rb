b788869ff4387c53934f705c41b75b8b86e8fdab
Sveta Krasikova <contact@svetakrasikova.eu>
2014-10-29 22:16:18 +0100

    add invite button and link it to create in partnerships controller

--- app/controllers/partnerships_controller.rb
+++ app/controllers/partnerships_controller.rb
@@ -10,16 +10,16 @@ class PartnershipsController < ApplicationController
   end
 
 
-  # POST /types
-  # POST /types.json
-
+  # POST /partnership
+  # POST /partnership
   def create
-    @partnership = Partnership.new(partnership_params)
+    @partnership = Partnership.new
     @partnership.user1_id = current_user.id
-
+    @partnership.user2_id = params[:user2_id]
+    friend_name = User.find(@partnership.user2_id).username
     respond_to do |format|
       if @partnership.save
-        format.html { redirect_to users_friends_path, notice: ' A new friend was successfully added.' }
+        format.html { redirect_to users_friends_path, notice: " An invitation was sent to #{friend_name}!" }
         format.json { render :show, status: :created, location: @type }
       else
         format.html { render :new }
