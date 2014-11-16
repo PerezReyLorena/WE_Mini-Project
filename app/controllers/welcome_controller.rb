2f0cc73c5c325d7af35030ab907967bd3531c9ee
Lorena Pérez Rey <lorena.perezrey@gmail.com>
2014-11-16 16:31:50 +0100

    rules in welcome controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,11 @@
 class WelcomeController < ApplicationController
+  skip_before_action :authorize
   def index
     if current_user.present?
       render 'welcome/home'
     end
+  end
+  def rules
+
   end
 end
