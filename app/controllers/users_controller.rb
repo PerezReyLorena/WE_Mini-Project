6959f50cb7f7b2e857a36a627b4c9a29e2501545
Sveta Krasikova <contact@svetakrasikova.eu>
2014-12-07 21:26:54 +0100

    config for deployment on heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
     partnerships = Partnership.where('(user1_id = ? OR user2_id = ?) AND confirmed = ?', current_user.id, current_user.id, true)
     # map them to the games
     game_ids = partnerships.map {|p| p.game_id}
-    @current_games = Game.where("id IN (#{game_ids.join(', ')}) AND end IS NULL")
+    @current_games = Game.where("id IN (#{game_ids.join(', ')}) AND 'end' IS NULL")
   end
 
 
